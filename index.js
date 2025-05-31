1758480a4815635813049f271acb84f6ceaa5ea4
Joyel <Varghesejoyel71@gmail.com>
2025-05-31 15:19:41 +0530

    Updated Frontend URL

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://e-commerce-store-dusky.vercel.app', 
+}));
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
